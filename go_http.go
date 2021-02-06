9382e196c8a18768fec5b4fb889f1aeac7d95f95
bbrlgvvps <bbrlgvvps@gmail.com>
2021-02-06 11:55:41 +0000

    premier commit

---
+++ go_http.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+    "fmt"
+    "log"
+    "net/http"
+    "io/ioutil"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprintf(w, "<h1>Fluubi's shitty website (Covide 666)</h1>")
+	fmt.Fprintf(w, "Hello, bienvenu à la racine, ceci est un test de serveur web en GoLang! ")
+}
+
+
+type Page struct {
+
+	Title string
+
+	Body  []byte
+
+}
+
+func (p *Page) save() error {
+
+	filename := p.Title + ".txt"
+
+	return ioutil.WriteFile(filename, p.Body, 0600)
+
+}
+
+
+func loadPage(title string) (*Page, error) {
+
+	filename := title + ".txt"
+
+	body, err := ioutil.ReadFile(filename)
+
+	if err != nil {
+
+		return nil, err
+
+	}
+
+	return &Page{Title: title, Body: body}, nil
+
+}
+
+///Cette fonction répond à tous les appels avec voir dans le nom
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+    title := r.URL.Path[len("/voir/"):]
+    p, _ := loadPage(title)
+    fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+}
+
+
+func main() {
+    http.HandleFunc("/", handler)
+    http.HandleFunc("/voir/", viewHandler)
+    log.Fatal(http.ListenAndServe(":1111", nil))
+}
